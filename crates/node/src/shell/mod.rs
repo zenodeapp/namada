d55f99a56772268abd8b2f03699b7cd7525dfd74
Tomáš Zemanovič <tomas@heliax.dev>
2024-07-03 09:02:45 +0100

    shell/mempool: skip chain ID check for fuzzing

--- crates/node/src/shell/mod.rs
+++ crates/node/src/shell/mod.rs
@@ -1000,6 +1000,7 @@ where
         };
 
         // Tx chain id
+        #[cfg(not(fuzzing))]
         if tx.header.chain_id != self.chain_id {
             response.code = ResultCode::InvalidChainId.into();
             response.log = format!(
